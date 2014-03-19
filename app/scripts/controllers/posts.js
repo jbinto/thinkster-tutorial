b2a6e633e8ef09b85f483ea53ca931f87fc016bf
Jesse Buchanan <jbinto@jbinto.ca>
2014-03-19 19:51:30 -0400

    Add form, submitting pushes values to an array

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,6 +1,15 @@
 'use strict';
 
-app.controller('PostsCtrl', function() {
+app.controller('PostsCtrl', function($scope) {
+  $scope.posts = [];
 
+  // "Empty" post. Will be used for in the <form> tag.
+  $scope.post = { url: 'http://', title: '' };
+
+  $scope.submitPost = function() {
+    // $scope.post is "updated" in real-time, i.e. two-way binding.
+    $scope.posts.push($scope.post);
+    $scope.post = { url: 'http://', title: '' };
+  };
 });
 
