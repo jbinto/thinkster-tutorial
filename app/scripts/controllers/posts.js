223b97b2a599fa60dba9ff3073f3ea7b453f19db
Jesse Buchanan <jbinto@jbinto.ca>
2014-03-20 14:27:04 -0400

    Move submitPost to new NavCtrl

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -7,12 +7,6 @@ app.controller('PostsCtrl', function($scope, $location, Post) {
   // "Empty" post. Will be used for in the <form> tag.
   $scope.post = { url: 'http://', title: '' };
 
-  $scope.submitPost = function() {
-    Post.create($scope.post).then(function(ref) {
-      $location.path('/posts/' + ref.name());
-    });
-  };
-
   $scope.deletePost = function(postId) {
     Post.delete(postId);
   };
