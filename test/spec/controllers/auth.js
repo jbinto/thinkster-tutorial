00c330eda6aaa32c0d4108205bfd815dfa3b3644
Jesse Buchanan <jbinto@jbinto.ca>
2014-03-20 19:14:36 -0400

    Make karma happy

--- test/spec/controllers/auth.js
+++ test/spec/controllers/auth.js
@@ -3,7 +3,7 @@
 describe('Controller: AuthCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('thinksterTutorialApp'));
+  beforeEach(module('angNewsApp'));
 
   var AuthCtrl,
     scope;
