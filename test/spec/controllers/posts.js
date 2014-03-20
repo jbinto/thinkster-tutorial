2fe1020595624bacb21df32ac6fde109174d2514
Jesse Buchanan <jbinto@jbinto.ca>
2014-03-19 20:08:00 -0400

    Ressurect unit test from e95f36f, karma doesn't like having 0 tests.

---
+++ test/spec/controllers/posts.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Controller: PostsCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('angNewsApp'));
+
+  var PostsCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    PostsCtrl = $controller('PostsCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach an empty post to the scope', function () {
+    expect(scope.post.url).toEqual("http://");
+    expect(scope.post.title).toEqual("");
+  });
+});
