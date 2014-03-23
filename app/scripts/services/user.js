7efec8b74f2739dfd4da2717aa0bc4b859ca5326
Jesse Buchanan <jbinto@jbinto.ca>
2014-03-23 17:46:54 -0400

    Show logged in user in nav

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.service('User', function($firebase, FIREBASE_URL) {
+app.service('User', function($firebase, FIREBASE_URL, $rootScope) {
   var ref = new Firebase(FIREBASE_URL + 'users');
 
   var users = $firebase(ref);
@@ -9,14 +9,58 @@ app.service('User', function($firebase, FIREBASE_URL) {
     create: function(authUser, username) {
       /*jshint camelcase: false */
       users[username] = {
-        md5Hash: authUser.md5_hash,
+        md5Hash: authUser.md5_hash,   // for Gravatar
         username: username,
         $priority: authUser.uid   // firebase specific: like PK
       };
 
-      users.$save(username);
+      users.$save(username).then(function () {
+        setCurrentUser(username);
+      });
+    },
+
+    findByUsername: function(username) {
+      if (username) {
+        // $child returns all child objects of 'users' collection
+        // Remember, this is a JSON database
+        return users.$child(username);
+      }
+    },
+
+    signedIn: function() {
+      return $rootScope.currentUser !== undefined;
+    },
+
+    getCurrent: function() {
+      return $rootScope.currentUser;
     }
   };
+
+  function setCurrentUser(username) {
+    // Get the User object corresponding to 'username',
+    // and assign it globally to the root scope
+    $rootScope.currentUser = User.findByUsername(username);
+  }
+
+  // Handle login events from Firebase
+  $rootScope.$on('$firebaseSimpleLogin:login', function(e, user) {
+    debugger;
+
+    // Grab the user by uid (awkward)
+    var query = $firebase(ref.startAt(user.uid).endAt(user.uid));
+
+    // When query returns a result, set the current user
+    query.$on('loaded', function() {
+      var username = query.$getIndex()[0];
+      setCurrentUser(username);
+    });
+  });
+
+  // Remove user from scope when Firebase logs us out
+  $rootScope.$on('$firebaseSimpleLogin:logout', function() {
+    console.log('bye!');
+    delete $rootScope.currentUser;
+  });
 
   return User;
 });
